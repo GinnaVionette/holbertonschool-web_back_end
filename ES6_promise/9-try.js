79657fa584cb1cbc6be621a310ca119d3f234f03
GinnaVionette <9011@holbertonstudents.com>
2024-07-24 12:28:20 -0400

    fix spacing

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,15 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    let value;
-  
-    try {
-      value = mathFunction();
-    } catch (err) {
-      value = err.toString();
-    }
-  
-    queue.push(value);
-    queue.push('Guardrail was processed');
-  
-    return queue;
+  const queue = [];
+  let value;
+  try {
+    value = mathFunction();
+  } catch (err) {
+    value = err.toString();
   }
+  queue.push(value);
+  queue.push('Guardrail was processed');
+  return queue;
+}
